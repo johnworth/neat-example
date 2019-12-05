afde89a26b0a7e77fc325444352a97142c0ed163
John Wregglesworth <wregglej@cyverse.org>
2019-12-05 13:40:00 -0700

    Moving to a custom server using express

---
+++ server/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import next from 'next';
+
+const dev: boolean = process.env.NODE_ENV !== 'production';
+const port: number = parseInt(process.env.PORT || '3000', 10);
+
+const app = next({dev});
+const handle = app.getRequestHandler();
+
+app.prepare()
+    .then(()=> {
+        const server = express();
+
+        server.get('*', (req, res) => {
+            return handle(req, res);
+        });
+
+        server.listen(port, (err) => {
+            if (err) throw err;
+            console.log(`> Ready on http://localhost:${port}`);
+        });
+    })
+    .catch(exception => {
+        console.error(exception.stack);
+        process.exit(1);
+    });
