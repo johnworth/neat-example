2a14531ad77eb09ef0c8cd3a09194d204f86c454
John Wregglesworth <wregglej@iplantcollaborative.org>
2019-12-09 16:03:06 -0700

    Clean up types in the User resolver

--- common/types/UserInfo.ts
+++ common/types/UserInfo.ts
@@ -1,10 +1,42 @@
-export default interface UserInfo {
+
+export interface Workspace {
     id: string;
-    name: string;
-    firstName: string;
-    lastName: string;
-    email: string;
-    institution: string;
-    sourceID: string;
-    username: string;
+    root_category_id?: string;
+    is_public?: boolean;
+    new_workspace?: boolean;
+}
+
+export interface SystemIDs {
+    de_system_id?: string;
+    all_system_ids?: string[];
+}
+
+export interface WebhookType {
+    id?: string;
+    type?: string;
+    template?: string;
+}
+
+export interface Webhook {
+    id?: string;
+    url?: string;
+    topics?: [String];
+    type?: WebhookType
+}
+
+export default interface UserInfo {
+    id?: string;
+    name?: string;
+    first_name?: string;
+    last_name?: string;
+    email?: string;
+    institution?: string;
+    source_id?: string;
+    username?: string;
+    session?: any;
+    saved_searches?: any;
+    preferences?: any;
+    workspace?: Workspace;
+    system_ids?: SystemIDs;
+    webhooks?: Webhook[];
 };
--- server/data/resolvers/User.ts
+++ server/data/resolvers/User.ts
@@ -1,46 +1,84 @@
+import UserInfo, * as User from '../../../common/types/UserInfo';
+
 export default {
     User: {
-        // id: async (user, _args, { dataSources }) => {
-        //     return dataSources.apps.getUserInfo(user.username)
-        // },
-
-        saved_searches: async (user: any, _args: any, { dataSources }:{ dataSources: any }) => {
+        saved_searches: async (
+            user: UserInfo, 
+            _args: any, 
+            { dataSources }:{ dataSources: any }
+        ): Promise<string> => {
             return dataSources.deDB.getUserSavedSearches(user.id);
         },
 
-        session: async (user: any, _args: any, { dataSources }:{ dataSources: any }) => {
+        session: async (
+            user: UserInfo, 
+            _args: any, 
+            { dataSources }:{ dataSources: any }
+        ): Promise<any>=> {
             return dataSources.deDB.getUserSession(user.id);
         },
 
-        preferences: async (user: any, _args: any, { dataSources }:{ dataSources: any }) => {
+        preferences: async (
+            user: UserInfo, 
+            _args: any, 
+            { dataSources }:{ dataSources: any }
+        ): Promise<any> => {
             return dataSources.deDB.getUserPreferences(user.id);
         },
 
-        webhooks: async (user: any, _args: any, { dataSources }:{ dataSources: any }) => {
+        webhooks: async (
+            user: UserInfo, 
+            _args: any, 
+            { dataSources }:{ dataSources: any }
+        ): Promise<User.Webhook> => {
             return dataSources.apps.getUserWebhooks(user.username);
         },
 
-        apps: async (user: any, _args: any, { dataSources }:{ dataSources: any }) => {
+        apps: async (
+            user: UserInfo, 
+            _args: any, 
+            { dataSources }:{ dataSources: any }
+        ): Promise<any[]> => {
             return dataSources.apps.getAccessibleApps(user.username);
         },
 
-        workspace: async (user: any, _args: any, { dataSources }:{ dataSources: any }) => {
+        workspace: async (
+            user: UserInfo, 
+            _args: any, 
+            { dataSources }:{ dataSources: any }
+        ): Promise<User.Workspace> => {
             return dataSources.apps.getWorkspace(user.username);
         },
 
-        system_ids: async (user: any, _args: any, { dataSources }:{ dataSources: any }) => {
+        system_ids: async (
+            user: UserInfo, 
+            _args: any, 
+            { dataSources }:{ dataSources: any }
+        ): Promise<User.SystemIDs> => {
             return dataSources.apps.getSystemIDs(user.username);
         },
 
-        analyses: async (user: any, _args: any, { dataSources }:{ dataSources: any }) => {
+        analyses: async (
+            user: UserInfo, 
+            _args: any, 
+            { dataSources }:{ dataSources: any }
+        ): Promise<any[]> => {
             return dataSources.deDB.analysesLookupsByUser(user.username);
         },
 
-        tools: async (user: any, _args: any, { dataSources }:{ dataSources: any }) => {
+        tools: async (
+            user: UserInfo, 
+            _args: any, 
+            { dataSources }:{ dataSources: any }
+        ): Promise<any[]> => {
             return dataSources.apps.getTools(user.username);
         },
 
-        avus: async (user: any, _args: any, { dataSources }:{ dataSources: any }) => {
+        avus: async (
+            user: UserInfo, 
+            _args: any, 
+            { dataSources }:{ dataSources: any }
+        ): Promise<any[]> => {
             return dataSources.metadataDB.getAVUs('user', user.id);
         },
     },
