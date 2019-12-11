255675b4bee3b49cdbacb6ea6722117208ad251e
John Wregglesworth <wregglej@iplantcollaborative.org>
2019-12-11 09:12:40 -0700

    Move type definitions into the typeDefs directory

--- server/data/typeDefs/index.ts
+++ server/data/typeDefs/index.ts
@@ -1,15 +1,13 @@
-import { 
-    userType, 
-    appType, 
-    analysisType, 
-    toolType,
-    permissionType,
-    AVUType,
-    templateType,
-    commentType,
-} from '../typeDefs/types';
+import userType from './User';
+import appType from './App';
+import analysisType from './Analysis';
+import toolType from './Tool';
+import permissionType from './Permission';
+import AVUType from './AVU';
+import templateType from './Template';
+import commentType from './Comment';
 
-import query from '../typeDefs/query';
+import query from './query';
 
 export default [
     query, 
