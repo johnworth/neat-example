af13a0989b8b88c876326dc97aad2a5453dc007a
John Wregglesworth <wregglej@iplantcollaborative.org>
2019-12-11 11:00:22 -0700

    Remove debug logging from the camelcase middleware

--- server/graphql/middleware/camelCase.ts
+++ server/graphql/middleware/camelCase.ts
@@ -4,8 +4,6 @@ export default {
     Query: {
         templates: async (resolve: any, parent: any, args: any, context:any, info: any) => {
             const result = await resolve(parent, args, context, info);
-            console.log(result);
-            console.log(camelcaseit(result));
             return camelcaseit(result);
         }
     }
